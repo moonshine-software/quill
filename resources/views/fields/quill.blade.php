144add808b6ae910513bac1db25521713a09e93d
Danil Shutsky <leetoplayaz@yandex.ru>
2024-05-15 17:18:21 +0300

    Hot fix
    added textarea

--- resources/views/fields/quill.blade.php
+++ resources/views/fields/quill.blade.php
@@ -1,15 +1,40 @@
-<div>
-    <div id="quill_{{ $element->id() }}" style="height: auto;">
-        {!! $value ?? '' !!}
-    </div>
+<div x-data="quill">
+    <div class="ql-editor" :id="$id('quill')" style="height: auto;">{!! $value ?? '' !!}</div>
+
+    <x-moonshine::form.textarea
+        :attributes="$element->attributes()->merge([
+            'class' => 'ql-textarea',
+            'name' => $element->name(),
+            'style' => 'display: none;'
+        ])->except('x-bind:id')"
+    >{!! $value ?? '' !!}</x-moonshine::form.textarea>
 </div>
 
 <script>
-    document.addEventListener("DOMContentLoaded", () => {
-        let quill = new Quill('#quill_{{ $element->id() }}', {
-            theme: 'snow'
-        });
-    });
+    document.addEventListener('alpine:init', () => {
+        Alpine.data('quill', () => ({
+            textarea: null,
+            editor: null,
+
+            init() {
+                this.textarea = this.$root.querySelector('.ql-textarea')
+                this.editor = this.$root.querySelector('.ql-editor')
+
+                const t = this
+
+                this.$nextTick(function() {
+                    let quill = new Quill(`#${t.editor.id}`, {
+                        theme: 'snow'
+                    });
+
+                    quill.on('text-change', () => {
+                        t.textarea.value = t.editor.innerHTML || '';
+                        t.textarea.dispatchEvent(new Event('change'));
+                    });
+                })
+            },
+        }))
+    })
 </script>
 
 <style>
